11bdc62198f3f238ab06066efca6ca22f3f93f2b
bsainarsareddy <sainarsabireddy@gmail.com>
2025-07-31 10:58:37 +0530

    Create ProductDetail.js

---
+++ ProductDetail.js
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/products/${id}`)
+      .then(res => setProduct(res.data))
+      .catch(err => console.error(err));
+  }, [id]);
+
+  if (!product) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>{product.name}</h2>
+      <p>₹{product.price}</p>
+      <p>{product.description}</p>
+    </div>
+  );
+}
+
+export default ProductDetail;
